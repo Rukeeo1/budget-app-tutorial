ab3f6e43500a0dbc839296fd54cbac475eb73354
Rukee Ojigbo <rukeeojigbo@gmail.com>
2020-11-01 21:34:49 +0100

    ch: add dummy data

---
+++ src/dummyData/categories.js
@@ -0,0 +1,23 @@
+import {v4} from 'uuid';
+export const categories = [
+  {
+    id: v4(),
+    body: 'Rent',
+  },
+  {
+    id: v4(),
+    body: 'Phone',
+  },
+  {
+    id: v4(),
+    body: 'Health Care',
+  },
+  {
+    id: v4(),
+    body: 'Insurance',
+  },
+  {
+    id: v4(),
+    body: 'Tax',
+  },
+];
